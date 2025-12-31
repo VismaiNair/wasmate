6fd3a092bae4622b30966a0716c4a2c5a99948a6
Vismai Nair <vismai.nair@gmail.com>
2025-12-30 21:13:45 -0700

    feat: use websocket for wasmate run

--- cmd/run.go
+++ cmd/run.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 
-	"github.com/vismainair/wasmate/browser"
-
+	"github.com/gorilla/websocket" // Run: go get github.com/gorilla/websocket
 	"github.com/spf13/cobra"
+	"github.com/vismainair/wasmate/browser"
 )
 
 var port int
 var open bool
+
+// WebSocket configuration
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+// Thread-safe storage for active browser connections
+var (
+	clients   = make(map[*websocket.Conn]bool)
+	clientsMu sync.Mutex
+)
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
@@ -20,16 +32,24 @@ var runCmd = &cobra.Command{
 	Long:  ``,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
+		mux := http.NewServeMux()
+
+		// 1. The standard file server
 		fs := http.FileServer(http.Dir("."))
-		http.Handle("/", fs)
+		mux.Handle("/", fs)
+
+		// 2. The WebSocket endpoint for hot-reload signals
+		mux.HandleFunc("/ws", handleWebSocket)
 
 		if open {
 			browser.Open(fmt.Sprintf("http://localhost:%d", port))
-			fmt.Fprintf(os.Stdout, "Opening the file in your browser")
+			fmt.Fprintf(os.Stdout, "Opening the file in your browser\n")
 		}
+
 		fmt.Fprintf(os.Stdout, "The WASM static webserver is starting on http://localhost:%d\n", port)
-		fmt.Fprintf(os.Stdout, "Press Ctrl+C or Command+C to stop the server.")
-		err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+		fmt.Fprintf(os.Stdout, "Press Ctrl+C or Command+C to stop the server.\n")
+
+		err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
 		if err != nil {
 			return fmt.Errorf("failed to start server: %w", err)
 		}
@@ -38,17 +58,48 @@ var runCmd = &cobra.Command{
 	},
 }
 
+// handleWebSocket upgrades the HTTP connection and tracks the client
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+
+	clientsMu.Lock()
+	clients[conn] = true
+	clientsMu.Unlock()
+
+	// Keep connection alive until client disconnects
+	defer func() {
+		clientsMu.Lock()
+		delete(clients, conn)
+		clientsMu.Unlock()
+		conn.Close()
+	}()
+
+	for {
+		if _, _, err := conn.ReadMessage(); err != nil {
+			break
+		}
+	}
+}
+
+// NotifyReload sends a reload message to all connected browser tabs
+func NotifyReload() {
+	clientsMu.Lock()
+	defer clientsMu.Unlock()
+
+	for client := range clients {
+		err := client.WriteMessage(websocket.TextMessage, []byte("reload"))
+		if err != nil {
+			client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
 func init() {
 	rootCmd.AddCommand(runCmd)
 	runCmd.Flags().IntVarP(&port, "port", "p", 8080, "The port to run the web server on")
 	runCmd.Flags().BoolVarP(&open, "open", "o", false, "Open the app on the default browser")
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
