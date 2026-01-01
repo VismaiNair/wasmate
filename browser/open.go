22de74aa396460b5d23a8cb3fa03fbf1c895f0be
Vismai Nair <vismai.nair@gmail.com>
2025-12-31 19:32:14 -0700

    docs: make browser/open docs more thorough

--- browser/open.go
+++ browser/open.go
@@ -6,6 +6,8 @@ import (
 )
 
 // Open() opens the default browser through the platform's cmd.
+// It returns an error if the command fails to start.
+// To be used by the wasmate run command to open the served files in the browser.
 func Open(url string) error {
 	var cmd *exec.Cmd
 
